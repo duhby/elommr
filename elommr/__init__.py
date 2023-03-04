12f579c1bba5c2fb651724d8707c3f8c435b541e
Josh <epikmemez@outlook.com>
2023-03-03 22:05:35 -0700

    Bump to 1.0.1

--- elommr/__init__.py
+++ elommr/__init__.py
@@ -12,6 +12,6 @@ __title__ = 'elommr'
 __author__ = 'duhby'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023-present duhby'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from .elommr import *
