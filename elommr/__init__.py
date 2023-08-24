b12b082ac4f83927c9a0abc4327efbba74552461
Josh <59372145+duhby@users.noreply.github.com>
2023-08-24 17:03:38 -0600

    Black formatting

--- elommr/__init__.py
+++ elommr/__init__.py
@@ -8,10 +8,10 @@ Copyright (c) 2023-present duhby
 MIT License, see LICENSE for more details.
 """
 
-__title__ = 'elommr'
-__author__ = 'duhby'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2023-present duhby'
-__version__ = '1.0.2'
+__title__ = "elommr"
+__author__ = "duhby"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present duhby"
+__version__ = "1.0.2"
 
 from .elommr import *
